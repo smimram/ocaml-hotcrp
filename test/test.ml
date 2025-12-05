76f45f2757c51c994b3921c8f632484360fe0403
Samuel Mimram <smimram@gmail.com>
2025-12-05 14:43:00 +0100

    Fix search.

--- test/test.ml
+++ test/test.ml
@@ -14,13 +14,13 @@ let main () =
   Printf.printf "Token: %s\n%!" token;
   let h = HotCRP.make url ~token in
   (* let* p = HotCRP.paper h 650 in *)
-  let* tags = HotCRP.tags h 993 in
+  (* let* tags = HotCRP.tags h 993 in *)
   (* let* () = HotCRP.add_tag h 993 "test" in *)
   (* let* _ = HotCRP.reviews h 993 in *)
   let* ids = HotCRP.search_ids h "" in
-  print_endline "Got paper!";
+  (* print_endline "Got paper!"; *)
   Printf.printf "ids: %s\n%!" (String.concat "," @@ List.map string_of_int ids);
-  Printf.printf "tags: %s\n%!" (String.concat ", " tags);
+  (* Printf.printf "tags: %s\n%!" (String.concat ", " tags); *)
   (* let* _ = HotCRP.add_comment h 993 ~text:"This is a test" () in *)
   return ()
 
